815ccca313cefa54ab3cb00e8606a05883036851
makaveli <agarwal.dhruv1234@gmail.com>
2024-08-15 10:53:48 -0500

    minimap added

---
+++ common/recoil/options/options.hooks.ts
@@ -0,0 +1,10 @@
+import { useRecoilValue } from "recoil"
+import { optionsAtom } from "./options.atoms"
+
+
+
+export const useOptions = () => {
+    const options = useRecoilValue(optionsAtom);
+
+    return options;
+};
