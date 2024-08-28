e76a11fb88511b93e245ce6a59944300f0b9f724
makaveli <agarwal.dhruv1234@gmail.com>
2024-08-28 00:00:20 -0500

    deploy?

---
+++ pages/api/platforms.ts
@@ -0,0 +1,10 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json([
+    { name: "Vercel", isComplete: true },
+    { name: "Heroku", isComplete: true },
+    { name: "DigitalOcean", isComplete: false },
+    { name: "AWS", isComplete: false },
+  ]);
+}
